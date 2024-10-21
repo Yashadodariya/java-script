5928471be83e26bf347dfd48476c6ac580df7d9e
yashadodariya <yashadodariya7418@gmail.com>
2024-10-21 09:45:18 +0530

    check

---
+++ funexpre&reg/q-1.js
@@ -0,0 +1,20 @@
+// question 1
+    function name(yash){
+
+        console.log(name)
+    }
+    name()
+
+
+    // question 2
+    const getRectArea = function (width, height) {
+     return 3*4;
+};
+
+console.log(getRectArea(3, 4));
+
+// question 3
+    function multi(a,b){
+        return 7*3;
+    }
+    console.log(multi(7,3));
